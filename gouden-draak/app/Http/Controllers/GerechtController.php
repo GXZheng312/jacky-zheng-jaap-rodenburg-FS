6dff89843ed77c302d00b876b00276a820d4534d
Jacky <j.zheng657@hotmail.com>
2021-06-13 01:58:56 +0200

    Comments re-added

--- gouden-draak/app/Http/Controllers/GerechtController.php
+++ gouden-draak/app/Http/Controllers/GerechtController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class GerechtController extends Controller
 {
+    /**
+        * Display a listing of the resource.
+        *
+        * @return Response
+        */
     public function index()
     {
         $gerechten = Gerecht::all();
@@ -14,16 +19,32 @@ class GerechtController extends Controller
         return view('app.cashier.gerecht.index', ['gerechten' => $gerechten]);
     }
 
+    /**
+        * Show the form for creating a new resource.
+        *
+        * @return Response
+        */
     public function create()
     {
         return view('app.cashier.gerecht.createOrEdit');
     }
 
+    /**
+        * Store a newly created resource in storage.
+        *
+        * @return Response
+        */
     public function store()
     {
         return view('app.cashier.gerecht.index', ['gerechten' => Gerecht::all()]);
     }
 
+    /**
+        * Display the specified resource.
+        *
+        * @param  int  $id
+        * @return Response
+        */
     public function show($id)
     {
         $gerecht = Gerecht::findOrFail($id);
@@ -32,6 +53,12 @@ class GerechtController extends Controller
         return view('app.cashier.gerecht.show', ['gerecht' => $gerecht]);
     }
 
+    /**
+        * Show the form for editing the specified resource.
+        *
+        * @param  int  $id
+        * @return Response
+        */
     public function edit($id)
     {
         $gerecht = Gerecht::findOrFail($id);
@@ -39,14 +66,25 @@ class GerechtController extends Controller
         return view('app.cashier.gerecht.createOrEdit', ['gerecht' => $gerecht]);
     }
 
-
+    /** 
+        * Update the specified resource in storage.
+        *
+        * @param  int  $id
+        * @return Response
+        */
     public function update($id)
     {
         $gerecht = Gerecht::findOrFail($id);
 
         return view('app.cashier.gerecht.createOrEdit', ['gerecht' => $gerecht]);
     }
-
+    
+    /**
+        * Remove the specified resource from storage.
+        *
+        * @param  int  $id
+        * @return Response
+        */
     public function destroy($id)
     {
         $gerechten = Gerecht::all();
