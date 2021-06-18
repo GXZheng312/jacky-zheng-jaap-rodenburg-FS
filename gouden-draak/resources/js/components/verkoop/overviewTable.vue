a79a02e64233b786ec5255053292b62af736b688
Jacky <j.zheng657@hotmail.com>
2021-06-18 20:04:08 +0200

    Bootstrap icons added

--- gouden-draak/resources/js/components/verkoop/overviewTable.vue
+++ gouden-draak/resources/js/components/verkoop/overviewTable.vue
@@ -1,11 +1,54 @@
 <template>
-  	<div>
-      	overview-table
-  	</div>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Bestelnummer</th>
+          <th>Datum</th>
+          <th>Excl. BTW</th>
+          <th>BTW</th>
+          <th>Totaal</th>
+          <th>Gerechten</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="bestelling in bestellingen" v-bind:key="bestelling.id">
+          <td>{{ bestelling.id }}</td>
+          <td>{{ bestelling.datum }}</td>
+          <td>&euro; {{ (totaal(bestelling) * (1 - btw)).toFixed(decimals) }}</td>
+          <td>&euro; {{ (totaal(bestelling) * btw).toFixed(decimals) }}</td>
+          <td>&euro; {{ totaal(bestelling).toFixed(decimals) }}</td>
+          <td>
+            <i v-if="activeSubInfo == bestelling.id" class="bi bi-arrow-down-circle"></i>
+            <i v-else class="bi bi-arrow-up-circle-fill"></i>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-	props: ['bestellingen'],
-}
+  data() {
+    return {
+      activeSubInfo: 0,
+      btw: 0.06,
+      decimals: 2,
+    };
+  },
+  props: ['bestellingen'],
+  watch: {
+    bestellingen: function (val) {},
+  },
+  methods: {
+    totaal(bestelling) {
+      let total = 0.0;
+      bestelling.orders.forEach(order => {
+        total += order.gerecht.prijs * order.aantal;
+      });
+      return total;
+    },
+  },
+};
 </script>
--- gouden-draak/resources/views/layouts/basic/head.blade.php
+++ gouden-draak/resources/views/layouts/basic/head.blade.php
@@ -14,6 +14,7 @@
 
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 {{-- @yield('addCss') --}}
 
