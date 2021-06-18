61fb28dcfd1b261df6be26e1678c3c7a221f6898
Jacky <j.zheng657@hotmail.com>
2021-06-18 21:16:05 +0200

    Table design finished

--- gouden-draak/resources/js/components/verkoop/overviewTable.vue
+++ gouden-draak/resources/js/components/verkoop/overviewTable.vue
@@ -1,35 +1,46 @@
 <template>
   <div>
-    <table>
+    <table class="table table-striped vertical-align-middle">
       <thead>
-        <tr>
-          <th>Bestelnummer</th>
-          <th>Datum</th>
-          <th>Excl. BTW</th>
-          <th>BTW</th>
-          <th>Totaal</th>
-          <th>Gerechten</th>
+        <tr class="table-primary">
+          <th scope="col">Bestelnummer</th>
+          <th scope="col">Datum</th>
+          <th scope="col">Excl. BTW</th>
+          <th scope="col">BTW</th>
+          <th scope="col">Totaal</th>
+          <th scope="col">Gerechten</th>
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="bestelling in bestellingen" v-bind:key="bestelling.id">
-          <td>{{ bestelling.id }}</td>
-          <td>{{ bestelling.datum }}</td>
-          <td>&euro; {{ (totaal(bestelling) * (1 - btw)).toFixed(decimals) }}</td>
-          <td>&euro; {{ (totaal(bestelling) * btw).toFixed(decimals) }}</td>
-          <td>&euro; {{ totaal(bestelling).toFixed(decimals) }}</td>
-          <td>
-            <i v-if="activeSubInfo == bestelling.id" class="bi bi-arrow-down-circle"></i>
-            <i v-else class="bi bi-arrow-up-circle-fill"></i>
-          </td>
-        </tr>
+      <tbody name="slide" is="transition-group">
+        <template v-for="bestelling in bestellingen">
+          <tr v-bind:key="bestelling.id">
+            <td scope="row">{{ bestelling.id }}</td>
+            <td>{{ bestelling.datum }}</td>
+            <td>&euro; {{ (totaal(bestelling) * (1 - btw)).toFixed(decimals) }}</td>
+            <td>&euro; {{ (totaal(bestelling) * btw).toFixed(decimals) }}</td>
+            <td>&euro; {{ totaal(bestelling).toFixed(decimals) }}</td>
+            <td>
+              <button class="btn btnic" @click="toggleSubinfo(bestelling.id)">
+                <i v-if="activeSubInfo == bestelling.id" class="bi bi-arrow-up-circle-fill"></i>
+                <i v-else class="bi bi-arrow-down-circle"></i>
+              </button>
+            </td>
+          </tr>
+          <tr v-bind:key="bestelling.id" v-if="activeSubInfo == bestelling.id" class="container-fluid">
+            <td colspan="7"> 
+              <sub-overview-table :orders="bestelling.orders"/>
+            </td>
+          </tr>
+        </template>
       </tbody>
     </table>
   </div>
 </template>
 
 <script>
+import subOverviewTable from './subOverviewTable.vue';
 export default {
+  components: { subOverviewTable },
   data() {
     return {
       activeSubInfo: 0,
@@ -49,6 +60,13 @@ export default {
       });
       return total;
     },
+    toggleSubinfo(bestellingnr){
+      if(bestellingnr == this.activeSubInfo) {
+        this.activeSubInfo = 0;
+      } else {
+        this.activeSubInfo = bestellingnr;
+      }
+    }
   },
 };
 </script>
---
+++ gouden-draak/resources/js/components/verkoop/subOverviewTable.vue
@@ -0,0 +1,33 @@
+<template>
+<table class="table table-info table-striped vertical-align-middle">
+    <thead>
+        <tr>
+            <th scope="col">menunummer</th>
+            <th scope="col">Naam</th>
+            <th scope="col">Prijs</th>
+            <th scope="col">Aantal</th>
+            <th scope="col">Subtotaal</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="order in orders" v-bind:key="order.id"> 
+            <td> {{ order.gerecht.menunummer }}{{ order.gerecht.menu_toevoeging }}</td>
+            <td> {{ order.gerecht.naam }}</td>
+            <td> &euro; {{ (order.gerecht.prijs * 1).toFixed(decimals) }}</td>
+            <td> {{ order.aantal }}</td>
+            <td> &euro; {{ (order.gerecht.prijs * order.aantal).toFixed(decimals) }}</td>
+        </tr>
+    </tbody>
+</table>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      decimals: 2,
+    };
+  },
+  props: ['orders'],
+};
+</script>
