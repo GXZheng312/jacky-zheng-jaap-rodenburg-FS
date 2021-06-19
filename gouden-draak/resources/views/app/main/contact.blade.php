2e7731af6232bcd7197c200b73c8bfea890b5471
Jacky <j.zheng657@hotmail.com>
2021-06-19 12:05:58 +0200

    Contact page styled

--- gouden-draak/resources/views/app/main/contact.blade.php
+++ gouden-draak/resources/views/app/main/contact.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.basic')
 
 @section('content')
-De Gouden Draak is eenvoudig te vinden, vlak bij het centrum, 5 minuten lopen achter het centraal station.
-
-Onderwijsboulevard 215, kamer OG112
-5223 DE 's-Hertogenbosch
+    <div class="text-center p-3 fs-5 fw-bold">
+        <p class="mb-3 mt-2">
+            De Gouden Draak is eenvoudig te vinden, vlak bij het centrum, 5 minuten lopen achter het centraal station.
+        </p>
+        <p>
+            Onderwijsboulevard 215, kamer OG112 <br />
+            5223 DE 's-Hertogenbosch
+        </p>
+    </div>
+    <div class="w-100 border">
+        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0"
+            marginwidth="0"
+            src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=nl&amp;q=Onderwijsboulevard%20215,+(Mijn%20bedrijfsnaam)&amp;t=&amp;z=17&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"/>
+    </div>
 @endsection
