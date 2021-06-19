62b009c31a63ef58cd40af0b7f2f0161e214ab76
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2021-06-19 10:37:43 +0200

    Sales spreadsheet

---
+++ gouden-draak/app/Console/Commands/DailyExcel.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+class DailyExcel extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sales:daily';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Make a sales excel file each day';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->createExcel();
+        return $this->info('Created the Excel File');
+    }
+
+    private function createExcel()
+    {
+        $spreadsheet = new Spreadsheet();
+        $sheet = $spreadsheet->getActiveSheet();
+        $restaurantOrders = Order::where('afhaaltijdstip', '=', null)->with('orders')
+            ->with('orders.gerecht')
+            ->with('orders.aanbieding')
+            ->get();
+
+        $takeAwayOrders = Order::where('afhaaltijdstip', '!=', null)->with('orders')
+            ->with('orders.gerecht')
+            ->with('orders.aanbieding')
+            ->get();
+
+        $this->addOrdersToSheet($sheet, $restaurantOrders);
+        $sheet = $spreadsheet->createSheet(1);
+        $this->addOrdersToSheet($sheet, $takeAwayOrders);
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save("public/sales/sales-" . Carbon::now()->format('Y-m-d') . ".xlsx");
+    }
+
+    private function addOrdersToSheet($sheet, $orders)
+    {
+        $highestCellIndex = 0;
+        $sheet->setCellValue('A1', 'orderID');
+        $sheet->setCellValue('B1', 'AfhaalTijd');
+        $sheet->setCellValue('C1', 'naam');
+        foreach ($orders as $index => $order) {
+            $index = $index + 2;
+            $cellIndex = 0;
+            $sheet->setCellValue($this->num2alpha($cellIndex++) . $index, $order->id);
+            $sheet->setCellValue($this->num2alpha($cellIndex++) . $index, $order->afhaaltijdstip);
+            $sheet->setCellValue($this->num2alpha($cellIndex++) . $index, $order->naam);
+            foreach ($order->orders as $sub_order) {
+                $sheet->setCellValue($this->num2alpha($cellIndex++) . $index, $sub_order->gerecht_id);
+                $sheet->setCellValue($this->num2alpha($cellIndex++) . $index, $sub_order->opmerking);
+                $sheet->setCellValue($this->num2alpha($cellIndex++) . $index, $sub_order->aanbieding_id);
+                $sheet->setCellValue($this->num2alpha($cellIndex++) . $index, $sub_order->aantal);
+            }
+            if ($cellIndex - 3 > $highestCellIndex) $highestCellIndex = $cellIndex - 3;
+        }
+        for ($i = 0; $i < $highestCellIndex / 4; $i++) {
+            $localindex = 3;
+            $cellIndex = $i * 4;
+            $sheet->setCellValue($this->num2alpha($cellIndex + $localindex++) . '1', 'gerecht_id_' . $i);
+            $sheet->setCellValue($this->num2alpha($cellIndex + $localindex++) . '1', 'opmerking_' . $i);
+            $sheet->setCellValue($this->num2alpha($cellIndex + $localindex++) . '1', 'aanbieding_id_' . $i);
+            $sheet->setCellValue($this->num2alpha($cellIndex + $localindex++) . '1', 'aantal_' . $i);
+        }
+    }
+
+    private function num2alpha($n)
+    {
+        $r = '';
+        for ($i = 1; $n >= 0 && $i < 10; $i++) {
+            $r = chr(0x41 + ($n % pow(26, $i) / pow(26, $i - 1))) . $r;
+            $n -= pow(26, $i);
+        }
+        return $r;
+    }
+}
--- gouden-draak/app/Console/Kernel.php
+++ gouden-draak/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DailyExcel;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,18 +14,18 @@ class Kernel extends ConsoleKernel
    * @var array
    */
   protected $commands = [
-    //
+    DailyExcel::class,
   ];
 
   /**
    * Define the application's command schedule.
    *
-   * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+       * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
    * @return void
    */
   protected function schedule(Schedule $schedule)
   {
-    // $schedule->command('inspire')->hourly();
+     $schedule->command('sales:daily')->dailyAt('00:01');
   }
 
   /**
