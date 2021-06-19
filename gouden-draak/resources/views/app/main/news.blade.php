3d1da66fe8a5a1c70e38e8cc50959c8875952007
Jacky <j.zheng657@hotmail.com>
2021-06-19 11:57:01 +0200

    news page styled

--- gouden-draak/resources/views/app/main/news.blade.php
+++ gouden-draak/resources/views/app/main/news.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.basic')
 
 @section('content')
-Door de Corona crisis is De Gouden Draak op het moment slechts beperkt open.
+<div class="align-middle text-center pt-5 pb-5 fs-5 fw-bold">
+Door de Corona crisis is De Gouden Draak op het moment slechts beperkt open. <br>
 Het restaurant-gedeelte is gesloten. U kan uw favoriete gerechten nog wel afhalen.
+</div>
 @endsection
